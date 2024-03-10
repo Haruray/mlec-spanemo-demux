88f61a0b2245f1be6aee9ad22e6a00d46320a6da
Safiq Faray <syafiqfaray2@gmail.com>
2024-03-10 19:57:04 +0700

    debug

--- src/MLEC/trainer/EarlyStopping.py
+++ src/MLEC/trainer/EarlyStopping.py
@@ -51,5 +51,5 @@ class EarlyStopping:
             print(
                 f"Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ..."
             )
-        torch.save(model.state_dict(), "models/" + self.cur_date + "_checkpoint.pt")
+        torch.save(model.state_dict(), "../models/" + self.cur_date + "_checkpoint.pt")
         self.val_loss_min = val_loss
