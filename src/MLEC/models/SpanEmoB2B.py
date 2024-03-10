c84ff255472fe8e340a7404f0de941e52470f242
Safiq Faray <syafiqfaray2@gmail.com>
2024-03-10 18:26:16 +0700

    debug

--- src/MLEC/models/SpanEmoB2B.py
+++ src/MLEC/models/SpanEmoB2B.py
@@ -80,7 +80,7 @@ class SpanEmoB2B(MLECModel):
             decoder_attention_mask=label_attention_masks,
         )
         # get logits
-        logits = self.ffn(outputs[0])
+        logits = self.ffn(outputs.hidden_states)
         # get probabilities of tokens
         # get the predictions
         y_pred = self.compute_pred(logits)
