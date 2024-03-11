097de8aa621b37ef02a48cf5d10103d9ec575870
Safiq Faray <syafiqfaray2@gmail.com>
2024-03-11 10:49:38 +0700

    debug

--- src/MLEC/models/SpanEmoB2B.py
+++ src/MLEC/models/SpanEmoB2B.py
@@ -86,7 +86,7 @@ class SpanEmoB2B(MLECModel):
             target_attention_masks.to(device),
         )
 
-        all_label_input_ids = all_label_input_ids.long().to(device)
+        all_label_input_ids = all_label_input_ids.to(device)
         label_idxs = label_idxs[0].long().to(device)
 
         outputs = self.model(
