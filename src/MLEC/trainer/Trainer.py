874dec94b5c8d66b690b6cdf6ed36e103c2b7396
Safiq Faray <syafiqfaray2@gmail.com>
2024-03-18 15:50:37 +0700

    add delta arg to trainer

--- src/MLEC/trainer/Trainer.py
+++ src/MLEC/trainer/Trainer.py
@@ -31,12 +31,13 @@ class Trainer(object):
         filename,
         col_names=[],
         corr_type: CorrelationType = CorrelationType.IDENTITY,
+        delta=0
     ):
         self.model = model
         self.train_data_loader = train_data_loader
         self.val_data_loader = val_data_loader
         self.filename = filename
-        self.early_stop = EarlyStopping(self.filename, patience=10, delta=0.01)
+        self.early_stop = EarlyStopping(self.filename, patience=10, delta=delta)
         self.correlations = Correlations(corr_type=corr_type, col_names=col_names)
         self.col_names = col_names
 
